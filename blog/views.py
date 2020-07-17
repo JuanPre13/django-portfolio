a9d6e4b1438d65c3229ce0f9b091dd9c4b5503ee
Juan <prendas52@gmail.com>
2020-07-17 11:52:24 -0600

    Blog detail
    Summary of post, pretty date, admin change name objects, show one post

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 
-# Create your views here.
+
+def all_blogs(request):
+    blogs = Blog.objects
+    return render(request, 'blog/allblogs.html',
+                            {'blogs':blogs} 
+    )
+def show_blog(request, id_blog):
+    blog = get_object_or_404(Blog, pk=id_blog)
+    return render(request, 'blog/post.html',
+                            {'blog':blog}    
+    )
